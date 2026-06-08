9ff2cdf6bc4007e5af9f0beedbe6627079572ee7
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-06-08 10:39:03 +0330

    Deploy latest changes to gh-pages: mobile app layout + SBC improvements

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'fc26-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(
+  caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    const clone = res.clone();
+    caches.open(CACHE).then(c => c.put(e.request, clone));
+    return res;
+  }))
+));
